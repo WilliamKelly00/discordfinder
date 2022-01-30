af3f91a80b72411a010767bc3fc756e783dfbaba
WilliamKelly00 <williammkelly00@gmail.com>
2022-01-30 13:34:35 -0500

    fixed errors

--- pages/findserver.js
+++ pages/findserver.js
@@ -1,7 +1,7 @@
-export default function FindServer{
+export default function FindServer(){
   return(
     <div>
-      <h1>Find a community!<h1>
+      <h1>Find a community!</h1>
     </div>
   );
 }
